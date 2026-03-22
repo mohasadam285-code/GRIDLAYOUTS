78192672dd996288a1e29b4132e918e73e9e85bb
Elham💕 <elhaamoha20@gmail.com>
2026-03-22 08:47:25 +0300

    commit

---
+++ src/gridlayout/Gridlayout.java
@@ -0,0 +1,45 @@
+package gridlayout;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Gridlayout {
+    JFrame frame;
+    JPanel panelOne;
+    JPanel panelTwo;
+    JPanel panelTree;
+
+    public Gridlayout() {
+        this.createJFrame();
+    }
+
+    public JFrame createJFrame() {
+        frame = new JFrame();
+        frame.setLayout(new GridLayout(1, 3));
+        frame.add(this.createJPanelOne());
+        frame.add(this.createJPanelTwo());
+        frame.add(this.createJPanelTree());
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        return frame;
+    }
+
+    public JPanel createJPanelOne() {
+        panelOne = new JPanel();
+        panelOne.setBackground(Color.BLUE);
+        return panelOne;
+    }
+
+    public JPanel createJPanelTwo() {
+        panelTwo = new JPanel();
+        panelTwo.setBackground(Color.magenta);
+        return panelTwo;
+    }
+
+    public JPanel createJPanelTree() {
+        panelTree = new JPanel();
+        panelTree.setBackground(Color.green);
+        return panelTree;
+    }
+}
---
+++ src/gridlayout/Main.java
@@ -0,0 +1,7 @@
+package gridlayout;
+
+public class Main {
+    public static void main(String[] args) {
+        new Gridlayout();
+    }
+}
